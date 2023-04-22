9a276c5e15e37cd9564517b576d21817e4df4ad3
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-04-22 17:47:11 -0400

    Add fields in LoginForm.jsx

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,6 +1,26 @@
-function LoginForm(){
+import { useContext } from 'react'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+import { GeneralContext } from '../../contexts/GeneralContext';
+
+
+function LoginForm() {
+    const { setIsLogged } = useContext(GeneralContext)
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setIsLogged(true)
+    }
+
     return (
-        <h1>Login form</h1>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <TextField id="outlined-basic" label="Username" variant="outlined" />
+                <TextField id="outlined-password-input" label="Password" type="password" autoComplete="current-password" />
+                <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>Sign Up</Button>
+            </form>
+        </div>
     );
 }
 
