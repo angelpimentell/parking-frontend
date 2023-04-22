ce0ac3bcd4dc4e2726cd30a4fef4abd85ea60ddf
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-04-22 16:57:10 -0400

    Change logic for GeneralContext.jsx

--- src/contexts/GeneralContext.jsx
+++ src/contexts/GeneralContext.jsx
@@ -1,3 +1,17 @@
 import { createContext } from 'react'
 
 export const GeneralContext = createContext();
+
+export function GeneralContextProvider(props) {
+    const [isLogged, setIsLogged] = useState([])
+
+    return (
+        <GeneralContext.Provider
+            value={{
+                isLogged,
+                setIsLogged
+            }}>
+            {props.children}
+        </GeneralContext.Provider>
+    )
+}
