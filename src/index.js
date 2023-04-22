6cd0d73006ff6e941f3a96a59e50c0702f3e1883
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-04-22 17:47:48 -0400

    Add GeneralContext in index.jsx

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import GeneralContextProvider from './contexts/GeneralContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GeneralContextProvider>
+      <App />
+    </GeneralContextProvider>
   </React.StrictMode>
 );
 
