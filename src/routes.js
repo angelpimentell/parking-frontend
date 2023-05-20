5f2ed3ecb73b8d6fa3f394a60a0db812c12a0f24
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-05-20 17:19:44 -0400

    Add routes.js

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+import Main from './components/common/Main';
+import LoginForm from './components/common/LoginForm';
+
+import TicketPlanForm from './components/parking/ticketPlan/TicketPlanForm';
+import TicketPlanTable from './components/parking/ticketPlan/TicketPlanTable';
+import TicketPlanFormView from './components/parking/ticketPlan/TicketPlanFormView';
+import { ticketPlanLoader } from './services/TicketPlanService';
+
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+  },
+  {
+    path: "/login",
+    element: <LoginForm />,
+  },
+  {
+    path: "/ticket-plan",
+    element: <TicketPlanForm />,
+  },
+  {
+    path: "/ticket-plans",
+    element: <TicketPlanTable />,
+  },
+    {
+    path: "/ticket-plans/:ticketPlanId",
+    element: <TicketPlanFormView />,
+    loader: ticketPlanLoader
+  },
+]);
+
+export default router;
